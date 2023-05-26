e4d054b49afda5078ca5b47a76b0711a14af12cc
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-05-26 22:34:34 +0800

    fnt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,19 @@
 variable "bucket_name" {
-    type = string
+  type = string
 }
 
 variable "allowed_methods" {
-    type = list(string)
+  type    = list(string)
+  default = ["GET", "HEAD"]
 }
 
 variable "cached_methods" {
-    type = list(string)
+  type    = list(string)
+  default = ["GET", "HEAD"]
 }
 
 variable "viewer_protocol_policy" {
-    type = string
+  type    = string
+  default = "allow-all"
 }
 

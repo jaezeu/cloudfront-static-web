f3d9e2429ed5b04094812f9591a0bd93415f0c8d
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-05-26 22:10:24 +0800

    Initial commit

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,45 @@
+data "aws_cloudfront_cache_policy" "example" {
+  name = "Managed-CachingOptimized"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_web.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = "S3Origin"
+  }
+
+  enabled             = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    cache_policy_id  = data.aws_cloudfront_cache_policy.example.id
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+	name                              = "${aws_s3_bucket.static_web.id}-oac"
+	description                       = ""
+	origin_access_control_origin_type = "s3"
+	signing_behavior                  = "always"
+	signing_protocol                  = "sigv4"
+}
